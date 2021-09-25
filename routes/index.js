6fea5a8c1eb277cc400447d8e01441ad5b6ce17a
iyozh <evzhenko1106@gmail.com>
2021-09-25 19:31:15 +0300

    fixing routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('*', (req, res) => {
+router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/math-network-frontend/build/index.html'));
 });
 
