9877956b2c29fab72374ea9554cd34edfc52249a
iyozh <evzhenko1106@gmail.com>
2021-10-17 00:56:11 +0300

    fixing storage 2

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, '../client/build')
+        cb(null, './public/data/uploads/')
     },
 
     filename: function(req, file, cb) {
